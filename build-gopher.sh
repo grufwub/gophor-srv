40bc0a937acf55d727a3d7b03967b204358da305
kim (grufwub) <grufwub@gmail.com>
2020-07-04 23:01:08 +0100

    improve gopher server build script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- build-gopher.sh
+++ build-gopher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-go build -o gophor.gopher main_gopher.go
+CC='x86_64-linux-musl-gcc' CGO_ENABLED=1 go build -trimpath -buildmode 'pie' -a -tags 'netgo' -ldflags '-s -w -extldflags "-static"' -o gophor.gopher main_gopher.go
